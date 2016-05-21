6a6d5ee5f685f7a0c471ece6d63e83024594d880
Ralf Hintersteininger <dev.rhintersteininger@gmail.com>
2016-05-21 20:01:50 +0200

    zxing library initialized + QR-Code reader implemented

--- app/src/main/java/com/hintersteininger/ralf/openwheels/LobbyActivity.java
+++ app/src/main/java/com/hintersteininger/ralf/openwheels/LobbyActivity.java
@@ -3,7 +3,11 @@ package com.hintersteininger.ralf.openwheels;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by ralf on 19.05.16.
@@ -26,5 +30,37 @@ public class LobbyActivity extends Activity{
     {
         Intent intent = new Intent(this, Preferences.class);
         startActivity(intent);
+    }
+
+    public void joinGame (View v)
+    {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null)
+        {
+            if (result.getContents() == null)
+            {
+                //Canceld scan
+            }
+            else
+            {
+                //Scanned
+                Log.d("LobbyActivity", result.getContents());
+            }
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
